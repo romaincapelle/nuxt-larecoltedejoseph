8543eeec66ee286a41cdc98fd9546f11cc424ef1
romaincapelle <romain.capelle@gmail.com>
2020-08-18 09:25:18 -0700

    added base content

--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,42 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
-        nuxt-larecoltedejoseph
+      <h1 class="title sm">
+        La Recolte de Joseph
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <p>
+        Offrons la possibilité de consommer un produit régional, sain et
+        pratique. Notre stratégie est de vous proposer un produit au coeur de la
+        tendance venu directement de notre terroir. Un packaging innovant et
+        différenciant Une marque à l’écoute des envies et des besoins des
+        consommateurs Afin d’optimiser les ventes, l'achat coup de coeur et la
+        fidélisation. Un dispositif régional qui s'inscrit dans la durée
+        Variété: Celtiane Poids net 2.5 kg Sac kr
+      </p>
+      <p>
+        C’est avant tout, une histoire de famille, le savoir-faire de la terre
+        transmis de génération en génération depuis plusieurs décennies.
+        Aujourd’hui, comme hier, nous sommes unis par la même passion, celle de
+        l’agriculture authentique et responsable, notre désir étant de vous
+        proposer des produits de qualité issus de notre terroir en respectant
+        les équilibres de la nature. Nos pommes de terre sont triées et
+        emballées directement à la ferme afin de vous assurer une qualité et une
+        fraicheur irréprochable pour vos clients. La variété de pommes de terre
+        Celtiane a de multiples atouts. Il s'agit d'une pomme de terre de
+        consommation à la chair ferme. Son tubercule régulier de forme allongée,
+        de très bonne qualité, se conserve très bien. La peau est fine et jaune
+        tandis que la chair est jaune pâle. De très bonne tenue à la cuisson,
+        ses tubercules ne demandent aucun épluchage et sont parfaits pour les
+        cuissons à l'eau ou à la vapeur. Avec son packaging novateur et
+        respectueux de l’environnement, notre produit optimisera les ventes de
+        votre rayon.
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
